f69c8e6ac1772f527c78e20263244fcb3f2ec7cf
Ferit <thegreetcoder@gmail.com>
2024-01-19 05:56:19 +0300

    else-if

---
+++ JS-Dersleri/Lvl-1/app.js
@@ -0,0 +1,41 @@
+//             Ders ortalaması bulmak
+
+
+// let vize1 = Number(prompt("Vize 1 notunuzu giriniz :"))
+// let vize2 = Number(prompt("Vize 2 notunuzu giriniz :"))
+
+// let final = Number(prompt("Final notunuzu giriniz :"))
+
+// let ortalama = vize1 * 0.3 +vize2*0.3 + final*0.4
+// console.log(ortalama);
+
+// if(ortalama>=60){
+//     alert("Dersten geçtiniz tebrikler :))")
+//     console.log("Dersten geçtiniz tebrikler :))");
+// }else{
+//     alert("Kaldınız geçmiş olsın =(")
+//     console.log("Kaldınız geçmiş olsın =(");
+// }
+
+let sayi1 = Number(prompt("Ders notunuzu giriniz : "))
+
+
+if  ( sayi1>=90 && sayi1<=100) {
+  console.log("Tebrikler A notu ile geçtiniz");
+}else if (sayi1>=80 && sayi1<90){
+    console.log("Tebrikler B notu ile geçtiniz");
+}else if (sayi1>=70 && sayi1<80){
+    console.log("Tebrikler C notu ile geçtiniz");
+}else if (sayi1>=60 && sayi1<70){
+    console.log("Tebrikler D notu ile geçtiniz");
+}else if (sayi1>=50 && sayi1<60){
+    console.log("Tebrikler E notu ile geçtiniz");
+}else if(sayi1<50 && sayi1>0){
+    console.log("Dersten kaldınız");
+}
+else{
+    alert("Yanlış sayi girdiniz")
+    console.log("YANLIŞ SAYI GRİDİNİZ");
+}
+
+
